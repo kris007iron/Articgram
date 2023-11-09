65b11ce80aa1a909d835031879710cddbaa50bd6
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2023-11-09 21:57:53 +0100

    Add react-native-web, react-dom, and @expo/webpack-config dependencies, and update App.js to display "Hello world!" on a black background with white text.

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App()
+{
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 32,
+  }
 });
